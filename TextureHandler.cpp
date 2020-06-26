d98fd3a4a9e67afa4314eb397574b51ece281e59
meanbean <ms04301@st.habib.edu.pk>
2020-06-26 20:04:56 +0500

    simplified texturehandler

--- TextureHandler.cpp
+++ TextureHandler.cpp
@@ -1,51 +1,49 @@
 #include "TextureHandler.hpp"
 
-bool TextureHandler::load(std::string fileName, std::string id, SDL_Renderer *pRenderer)
+SDL_Texture *TextureHandler::loadTexture(SDL_Renderer *renderer, const char *texture)
 {
-    SDL_Surface *pTempSurface = IMG_Load(fileName.c_str());
-    if (pTempSurface == 0)
-    {
-        return false;
-    }
+    SDL_Surface *pTempSurface = IMG_Load(texture);
 
-    SDL_Texture *pTexture = SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
+    SDL_Texture *finalTexture = SDL_CreateTextureFromSurface(renderer, pTempSurface);
     SDL_FreeSurface(pTempSurface);
-
-    // everything went ok, add the texture to list
-    if (pTexture != 0)
-    {
-        m_textureMap[id] = pTexture;
-        return true;
-    }
-    // reaching here means something went wrong
-    return false;
+    return finalTexture;
 }
 
-void TextureHandler::draw(std::string id, int x, int y, int width, int height, SDL_Renderer *pRenderer, SDL_RendererFlip flip)
-{
-    // source
-    SDL_Rect srcRect;
-    // destination
-    SDL_Rect destRect;
+// everything went ok, add the texture to list
+// if (pTexture != 0)
+// {
+//     m_textureMap[id] = pTexture;
+//     return true;
+// }
+// // reaching here means something went wrong
+// return false;
+//
 
-    srcRect.x = 0;
-    srcRect.y = 0;
-    srcRect.w = destRect.w = width;
-    srcRect.h = destRect.h = height;
-    destRect.x = x;
-    destRect.y = y;
-    SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect, &destRect, 0, 0, flip);
-}
+//oid TextureHandler::draw(std::string id, int x, int y, int width, int height, SDL_Renderer *pRenderer, SDL_RendererFlip flip)
+//
+//   // source
+//   SDL_Rect srcRect;
+//   // destination
+//   SDL_Rect destRect;
+//
+//   srcRect.x = 0;
+//   srcRect.y = 0;
+//   srcRect.w = destRect.w = width;
+//   srcRect.h = destRect.h = height;
+//   destRect.x = x;
+//   destRect.y = y;
+//   SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect, &destRect, 0, 0, flip);
+//
 
-void TextureHandler::drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *pRenderer, SDL_RendererFlip flip)
-{
-    SDL_Rect srcRect;
-    SDL_Rect destRect;
-    srcRect.x = width * currentFrame;
-    srcRect.y = height * (currentRow - 1);
-    srcRect.w = destRect.w = width;
-    srcRect.h = destRect.h = height;
-    destRect.x = x;
-    destRect.y = y;
-    SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect, &destRect, 0, 0, flip);
-}
+//xtureHandler::drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *pRenderer, SDL_RendererFlip flip)
+//
+//_Rect srcRect;
+//_Rect destRect;
+//Rect.x = width * currentFrame;
+//Rect.y = height * (currentRow - 1);
+//Rect.w = destRect.w = width;
+//Rect.h = destRect.h = height;
+//tRect.x = x;
+//tRect.y = y;
+//_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect, &destRect, 0, 0, flip);
+//
--- TextureHandler.hpp
+++ TextureHandler.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "game.hpp"
 #include "SDL.h"
 #include "SDL_image.h"
@@ -5,11 +6,6 @@
 class TextureHandler
 {
 public:
-    bool load(std::string fileName, std::string id, SDL_Renderer *pRenderer);
-    // draw
-    void draw(std::string id, int x, int y, int width, int height, SDL_Renderer *pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
-    // draw frame
-    void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-    std::map<std::string, SDL_Texture *> m_textureMap;
-};
+    //whatever texture to load
+    static SDL_Texture *LoadTexture(SDL_Renderer *renderer, const char *file);
+}
